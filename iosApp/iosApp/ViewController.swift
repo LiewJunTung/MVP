84a0c0b02e506b3d01ea396df343f57b6b157da9
liewjuntung <liewjuntung@gmail.com>
2019-06-20 00:41:12 +0800

    ios ui completed

--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -2,13 +2,23 @@ import UIKit
 import app
 
 class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = Proxy().proxyHello()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func withdraw(_ sender: Any) {
+    
+    }
+    
+    @IBAction func deposit(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var amount: UILabel!
 }
